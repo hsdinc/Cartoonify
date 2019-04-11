e445134aaf4f0be5db20539188ed390145c320a7
hfields <hfields@cs.hmc.edu>
2019-04-11 14:57:00 -0700

    Renamed app to application in application.py

--- application.py
+++ application.py
@@ -2,37 +2,37 @@ from flask import Flask, render_template, request, send_file, after_this_request
 from faceMorph import morph
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
+@application.route('/')
 def index():
         return render_template('home.html')
 
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('/cartoonify')
+@application.route('/cartoonify')
 def cartoonify(filename = None):
     return render_template('cartoonify.html')
 
 
-@app.route('/cartoonify', methods=['POST'])
+@application.route('/cartoonify', methods=['POST'])
 def upload_image():
     image = request.files['image']
-    f = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+    f = os.path.join(application.config['UPLOAD_FOLDER'], image.filename)
     image.save(f)
     f = morph(image.filename)
     return render_template('cartoonify.html', filename=f, init=True)
 
 
-@app.route('/cartoonify/<path:filename>', methods=['GET', 'POST'])
+@application.route('/cartoonify/<path:filename>', methods=['GET', 'POST'])
 def download_image(filename):
     file_handle = open(filename, 'r')
     @after_this_request
@@ -42,11 +42,11 @@ def download_image(filename):
             os.remove(filename)
             file_handle.close()
         except Exception as error:
-            app.logger.error("Error removing or closing downloaded file handle", error)
+            application.logger.error("Error removing or closing downloaded file handle", error)
         return response
     return send_file(filename, as_attachment=True, mimetype='image/jpg')
 
-@app.route('/cartoonify')
+@application.route('/cartoonify')
 def tryagain(filename):
     file_handle = open(filename, 'r')
     @after_this_request
@@ -55,10 +55,10 @@ def tryagain(filename):
             os.remove(filename)
             file_handle.close()
         except Exception as error:
-            app.logger.error("Error removing or closing downloaded file handle", error)
+            application.logger.error("Error removing or closing downloaded file handle", error)
         return response
     return render_template('cartoonify.html', init=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='ec2-54-193-120-97.us-west-1.compute.amazonaws.com')
+    application.run(debug=True)
