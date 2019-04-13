1134cea856ce62f0fab8c1c0067a8fb1e350eca8
hfields <hfields@cs.hmc.edu>
2019-04-13 05:56:24 -0700

    Changed application.py to app.py and renamed application to app inside

--- app.py
+++ app.py
@@ -2,37 +2,37 @@ from flask import Flask, render_template, request, send_file, after_this_request
 from faceMorph import resizeImage, createTextFile, morph
 import os
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
-application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@application.route('/')
+@app.route('/')
 def index():
         return render_template('home.html')
 
 
-@application.route('/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 
-@application.route('/cartoonify')
+@app.route('/cartoonify')
 def cartoonify(filename = None):
     return render_template('cartoonify.html')
 
 
-@application.route('/cartoonify', methods=['POST'])
+@app.route('/cartoonify', methods=['POST'])
 def upload_image():
     image = request.files['image']
-    f = os.path.join(application.config['UPLOAD_FOLDER'], image.filename)
+    f = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
     image.save(f)
     resizeImage(f)
 
     return render_template('addpoints.html', filename=f)
 
-@application.route('/addpoints/<path:filename>', methods=['POST'])
+@app.route('/addpoints/<path:filename>', methods=['POST'])
 def add_points(filename):
     # Find extra points from form
     extraPoints = []
@@ -47,11 +47,11 @@ def add_points(filename):
 
     return render_template('cartoonify.html', filename = f, init = True)
 
-@application.route('/addpoints/<path:filename>', methods=['GET'])
+@app.route('/addpoints/<path:filename>', methods=['GET'])
 def add_points_image(filename):
     return send_file(filename, as_attachment=True, mimetype='image/jpg')
 
-@application.route('/cartoonify/<path:filename>', methods=['GET', 'POST'])
+@app.route('/cartoonify/<path:filename>', methods=['GET', 'POST'])
 def download_image(filename):
     #file_handle = open(filename, 'r')
     #text_file_handle = open(filename + ".txt", 'r')
@@ -63,11 +63,11 @@ def download_image(filename):
     #        file_handle.close()
     #        text_file_handle.close()
     #    except Exception as error:
-    #        application.logger.error("Error removing or closing downloaded file handle" + str(error))
+    #        app.logger.error("Error removing or closing downloaded file handle" + str(error))
     #    return response
     return send_file(filename, as_attachment=True, mimetype='image/gif')
 
-@application.route('/cartoonify')
+@app.route('/cartoonify')
 def tryagain(filename):
     file_handle = open(filename, 'r')
     text_file_handle = open(filename + ".txt", 'r')
@@ -79,10 +79,10 @@ def tryagain(filename):
             file_handle.close()
             text_file_handle.close()
         except Exception as error:
-            application.logger.error("Error removing or closing downloaded file handle" + str(error))
+            app.logger.error("Error removing or closing downloaded file handle" + str(error))
         return response
     return render_template('cartoonify.html', init=True)
 
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
